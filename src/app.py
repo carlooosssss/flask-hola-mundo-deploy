23a65e43550f43ba107d3abd4f4a8e68aae3538e
Carlos <carlos@gmail.com>
2025-02-03 13:13:10 +0100

    Añadido el requeriments.txt

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask 
+from dotenv import load_dotenv
+import os
+
+# load_dotenv()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    username = os.getenv('USER')
+    password = os.getenv('PASSWORD')
+    email = os.getenv('EMAIL')
+    print(username, password, email)
+    return 'MI PRIMERA APP DEPLOYEADA EN RENDER<br> {} {} {}'.format(username, password, email)
+
+
+def status_404(error):
+    return 'PAGE NOT FOUND ERROR 404'
+
+if __name__ == '__main__':
+    app.register_error_handler(404, status_404)
+    app.run(debug=True)
